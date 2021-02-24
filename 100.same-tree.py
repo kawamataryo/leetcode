5b091aad1da353a3e55b7233f7753dcbf9817def
kawamataryou <ba068082@gmail.com>
2021-02-25 07:40:35 +0900

    [from now] 2021/02/25 07:40:35
    diff --git a/100.same-tree.py b/100.same-tree.py
    new file mode 100644
    index 0000000..34de1c3
    --- /dev/null
    +++ b/100.same-tree.py
    @@ -0,0 +1,23 @@
    +#
    +# @lc app=leetcode id=100 lang=python3
    +#
    +# [100] Same Tree
    +#
    +
    +# @lc code=start
    +# Definition for a binary tree node.
    +# class TreeNode:
    +#     def __init__(self, val=0, left=None, right=None):
    +#         self.val = val
    +#         self.left = left
    +#         self.right = right
    +class Solution:
    +    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
    +        if not p and not q:
    +            return True
    +        if not q or not p:
    +            return False
    +        if p.val != q.val:
    +            return False
    +        return self.isSameTree(p.right, q.right) and self.isSameTree(p.left, q.left)
    +# @lc code=end
    diff --git a/88.merge-sorted-array.py b/88.merge-sorted-array.py
    new file mode 100644
    index 0000000..13ac703
    --- /dev/null
    +++ b/88.merge-sorted-array.py
    @@ -0,0 +1,21 @@
    +#
    +# @lc app=leetcode id=88 lang=python3
    +#
    +# [88] Merge Sorted Array
    +#
    +
    +# @lc code=start
    +class Solution:
    +    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
    +        """
    +        Do not return anything, modify nums1 in-place instead.
    +        """
    +        while m > 0 and n > 0:
    +            if nums1[m - 1] > nums2[n - 1]:
    +                nums1[m + n - 1] = nums1[m - 1]
    +                m -= 1
    +            else:
    +                nums1[m + n - 1] = nums2[n - 1]
    +                n -= 1
    +        nums1[:n] = nums2[:n]
    +# @lc code=end

---
+++ 100.same-tree.py
@@ -0,0 +1,23 @@
+#
+# @lc app=leetcode id=100 lang=python3
+#
+# [100] Same Tree
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+        if not p and not q:
+            return True
+        if not q or not p:
+            return False
+        if p.val != q.val:
+            return False
+        return self.isSameTree(p.right, q.right) and self.isSameTree(p.left, q.left)
+# @lc code=end
