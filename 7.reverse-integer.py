1275c7dd780776f2ee9daf9532ea13867440bf09
kawamataryou <ba068082@gmail.com>
2021-03-02 05:45:54 +0900

    [from now] 2021/03/02 05:45:54
    diff --git a/7.reverse-integer.py b/7.reverse-integer.py
    index 808646e..cd96447 100644
    --- a/7.reverse-integer.py
    +++ b/7.reverse-integer.py
    @@ -6,18 +6,9 @@
    
     # @lc code=start
     class Solution:
    -    def reverse(self, x: int) -> int:
    -        if x < 0:
    -            an1 = -self._reverse_numbers(-x)
    -            if an1 < -2**31:
    -                return 0
    -            return an1
    -        an2 = self._reverse_numbers(x)
    -        if an2 > 2**31 - 1:
    -            return 0
    -        return an2
    -
    -    def _reverse_numbers(self, x):
    -        return int(''.join(list(str(x))[::-1]))
    +    def reverse(self, x):
    +        s = 1 if x >= 0 else -1
    +        r = int(f"{s*x}"[::-1])
    +        return s*r if r < 2**31 else 0
    
     # @lc code=end

--- 7.reverse-integer.py
+++ 7.reverse-integer.py
@@ -6,18 +6,9 @@
 
 # @lc code=start
 class Solution:
-    def reverse(self, x: int) -> int:
-        if x < 0:
-            an1 = -self._reverse_numbers(-x)
-            if an1 < -2**31:
-                return 0
-            return an1
-        an2 = self._reverse_numbers(x)
-        if an2 > 2**31 - 1:
-            return 0
-        return an2
-
-    def _reverse_numbers(self, x):
-        return int(''.join(list(str(x))[::-1]))
+    def reverse(self, x):
+        s = 1 if x >= 0 else -1
+        r = int(f"{s*x}"[::-1])
+        return s*r if r < 2**31 else 0
 
 # @lc code=end
