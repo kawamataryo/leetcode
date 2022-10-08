040def5a435040ac992da7c1fd618172cbdf31b0
kawamataryo <ba068082@gmail.com>
2022-10-08 20:28:19 +0900

    [from now] 2022/10/08 20:28:19
    diff --git a/.vscode/settings.json b/.vscode/settings.json
    new file mode 100644
    index 0000000..7a73a41
    --- /dev/null
    +++ b/.vscode/settings.json
    @@ -0,0 +1,2 @@
    +{
    +}
    \ No newline at end of file
    diff --git a/solutions/1.two-sum.py b/solutions/1.two-sum.py
    index 11f4043..23beaaa 100644
    --- a/solutions/1.two-sum.py
    +++ b/solutions/1.two-sum.py
    @@ -7,11 +7,41 @@
     # @lc code=start
     class Solution:
         def twoSum(self, nums: List[int], target: int) -> List[int]:
    -        hashmap = {}
    -        for i in range(len(nums)):
    -            hashmap[nums[i]] = i
    -        for i in range(len(nums)):
    -            complement = target - nums[i]
    -            if complement in hashmap and hashmap[complement] != i:
    -                return [i, hashmap[complement]]
    +        # case 1 greedy
    +        # --------------------
    +        # numsLength = len(nums)
    +        # for x in range(numsLength):
    +        #     for y in range(x + 1, numsLength):
    +        #         if target - nums[x] == nums[y]:
    +        #             return [x, y]
    +
    +        # case 2 two-path hashmap
    +        # --------------------
    +        # hashmap = {}
    +        # for i in range(len(nums)):
    +        #     hashmap[nums[i]] = i
    +        # for i in range(len(nums)):
    +        #     complement = target - nums[i]
    +        #     if complement in hashmap and hashmap[complement] != i:
    +        #         return [i, hashmap[complement]]
    +
    +        # case 3 list
    +        # --------------------
    +        for i, num in enumerate(nums):
    +            complement = target - num
    +            if complement in nums:
    +                cIndex = nums.index(complement)
    +                if i != cIndex:
    +                    return [i, cIndex]
    +
    +        # case 4 one-path hashmap
    +        # --------------------
    +        # hashmap = {}
    +        # for x in range(len(nums)):
    +        #     complement = target - nums[x]
    +        #     if complement in hashmap:
    +        #         return [x, hashmap[complement]]
    +        #     hashmap[nums[x]] = x
    +
    +
     # @lc code=end
    diff --git a/solutions/9.palindrome-number.py b/solutions/9.palindrome-number.py
    index 6ce7abc..6ac8e7d 100644
    --- a/solutions/9.palindrome-number.py
    +++ b/solutions/9.palindrome-number.py
    @@ -6,25 +6,5 @@
    
     # @lc code=start
     class Solution:
    -    # def isPalindrome(self, x: int) -> bool:
    -    #     # 回文かどうか判定する
    -    #     if x < 0:
    -    #         return False
    -    #     if x == self._reverse_int(x):
    -    #         return True
    -    #     return False
    -
    -    # def _reverse_int(self, x: int) -> int:
    -    #     return int(''.join(list(str(x))[::-1]))
    -
         def isPalindrome(self, x: int) -> bool:
    -        if x < 0 or x % 10 == 0 and x != 0:
    -            return False
    -
    -        revertNumber = 0
    -        while x > revertNumber:
    -            revertNumber = revertNumber * 10 + x % 10
    -            x //= 10
    -
    -        return x == revertNumber or x == revertNumber // 10
     # @lc code=end

--- solutions/1.two-sum.py
+++ solutions/1.two-sum.py
@@ -7,11 +7,41 @@
 # @lc code=start
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hashmap = {}
-        for i in range(len(nums)):
-            hashmap[nums[i]] = i
-        for i in range(len(nums)):
-            complement = target - nums[i]
-            if complement in hashmap and hashmap[complement] != i:
-                return [i, hashmap[complement]]
+        # case 1 greedy
+        # --------------------
+        # numsLength = len(nums)
+        # for x in range(numsLength):
+        #     for y in range(x + 1, numsLength):
+        #         if target - nums[x] == nums[y]:
+        #             return [x, y]
+
+        # case 2 two-path hashmap
+        # --------------------
+        # hashmap = {}
+        # for i in range(len(nums)):
+        #     hashmap[nums[i]] = i
+        # for i in range(len(nums)):
+        #     complement = target - nums[i]
+        #     if complement in hashmap and hashmap[complement] != i:
+        #         return [i, hashmap[complement]]
+
+        # case 3 list
+        # --------------------
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in nums:
+                cIndex = nums.index(complement)
+                if i != cIndex:
+                    return [i, cIndex]
+
+        # case 4 one-path hashmap
+        # --------------------
+        # hashmap = {}
+        # for x in range(len(nums)):
+        #     complement = target - nums[x]
+        #     if complement in hashmap:
+        #         return [x, hashmap[complement]]
+        #     hashmap[nums[x]] = x
+
+
 # @lc code=end
--- solutions/9.palindrome-number.py
+++ solutions/9.palindrome-number.py
@@ -6,25 +6,5 @@
 
 # @lc code=start
 class Solution:
-    # def isPalindrome(self, x: int) -> bool:
-    #     # 回文かどうか判定する
-    #     if x < 0:
-    #         return False
-    #     if x == self._reverse_int(x):
-    #         return True
-    #     return False
-
-    # def _reverse_int(self, x: int) -> int:
-    #     return int(''.join(list(str(x))[::-1]))
-
     def isPalindrome(self, x: int) -> bool:
-        if x < 0 or x % 10 == 0 and x != 0:
-            return False
-
-        revertNumber = 0
-        while x > revertNumber:
-            revertNumber = revertNumber * 10 + x % 10
-            x //= 10
-
-        return x == revertNumber or x == revertNumber // 10
 # @lc code=end
