f1a2dd797edca4ef11b66d7f0575f88890157004
kawamataryo <ba068082@gmail.com>
2022-10-09 09:28:25 +0900

    [from now] 2022/10/09 09:28:25
    diff --git a/solutions/9.palindrome-number.py b/solutions/9.palindrome-number.py
    index d37ff51..731c081 100644
    --- a/solutions/9.palindrome-number.py
    +++ b/solutions/9.palindrome-number.py
    @@ -13,11 +13,10 @@ class Solution:
                 return False
             stringX = str(x)
             length = len(stringX)
    +        halfLength = math.floor(length / 2)
    
             if length % 2 == 0:
    -            halfLength = int(length / 2)
                 return stringX[0:halfLength] == stringX[halfLength::][::-1]
             else:
    -            halfLength = math.floor(length / 2)
                 return stringX[0:halfLength] == stringX[halfLength + 1::][::-1]
     # @lc code=end

--- solutions/9.palindrome-number.py
+++ solutions/9.palindrome-number.py
@@ -13,11 +13,10 @@ class Solution:
             return False
         stringX = str(x)
         length = len(stringX)
+        halfLength = math.floor(length / 2)
 
         if length % 2 == 0:
-            halfLength = int(length / 2)
             return stringX[0:halfLength] == stringX[halfLength::][::-1]
         else:
-            halfLength = math.floor(length / 2)
             return stringX[0:halfLength] == stringX[halfLength + 1::][::-1]
 # @lc code=end
