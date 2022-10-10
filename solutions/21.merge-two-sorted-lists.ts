8ef3009a4f8bec0dee93fa0bc0be58d434b9e817
kawamataryo <ba068082@gmail.com>
2022-10-11 07:46:41 +0900

    [from now] 2022/10/11 07:46:41
    diff --git a/.vscode/settings.json b/.vscode/settings.json
    index 7a73a41..2c63c08 100644
    --- a/.vscode/settings.json
    +++ b/.vscode/settings.json
    @@ -1,2 +1,2 @@
     {
    -}
    \ No newline at end of file
    +}
    diff --git a/solutions/21.merge-two-sorted-lists.ts b/solutions/21.merge-two-sorted-lists.ts
    index 5dac439..62d40a5 100644
    --- a/solutions/21.merge-two-sorted-lists.ts
    +++ b/solutions/21.merge-two-sorted-lists.ts
    @@ -17,19 +17,53 @@
      }
    
     function mergeTwoLists(list1: ListNode | null, list2: ListNode | null): ListNode | null {
    -  const listNodeToList = (node: ListNode | null, result: number[]): number[] => {
    -    if (!node) {
    -      return result
    -    }
    -
    -    return listNodeToList(node, [...result, node.val])
    -  }
    -
       if(!list1) {
         return list2
       }
       if(!list2) {
         return list1
       }
    +
    +  const listNodeToList = (node: ListNode): number[] => {
    +    const _listNodeToList = (node: ListNode | null, result: number[]): number[] => {
    +      if (!node) {
    +        return result
    +      }
    +
    +      return _listNodeToList(node, [...result, node.val])
    +    }
    +
    +    return _listNodeToList(node, [])
    +  }
    +
    +  const listToNodeList = (list: number[]) => {
    +    const _listToNodeList = (list: number[], node: ListNode | null) => {
    +      if(list.length === 0) {
    +        return node
    +      }
    +
    +      if(!node) {
    +        node = new ListNode(list[0])
    +      } else {
    +        node.val = list[0]
    +      }
    +
    +      if (list.length === 1) {
    +        node.next = null
    +      } else {
    +        node.next = new ListNode()
    +      }
    +
    +      return _listToNodeList(list.slice(1), node.next)
    +    }
    +    const firstNode = new ListNode()
    +    _listToNodeList(list, firstNode)
    +
    +    return firstNode
    +  }
    +
    +
    +
    +  return listToNodeList([...listNodeToList(list1), ...listNodeToList(list2)].sort())
     };
     // @lc code=end

--- solutions/21.merge-two-sorted-lists.ts
+++ solutions/21.merge-two-sorted-lists.ts
@@ -17,19 +17,53 @@
  }
 
 function mergeTwoLists(list1: ListNode | null, list2: ListNode | null): ListNode | null {
-  const listNodeToList = (node: ListNode | null, result: number[]): number[] => {
-    if (!node) {
-      return result
-    }
-
-    return listNodeToList(node, [...result, node.val])
-  }
-
   if(!list1) {
     return list2
   }
   if(!list2) {
     return list1
   }
+
+  const listNodeToList = (node: ListNode): number[] => {
+    const _listNodeToList = (node: ListNode | null, result: number[]): number[] => {
+      if (!node) {
+        return result
+      }
+
+      return _listNodeToList(node, [...result, node.val])
+    }
+
+    return _listNodeToList(node, [])
+  }
+
+  const listToNodeList = (list: number[]) => {
+    const _listToNodeList = (list: number[], node: ListNode | null) => {
+      if(list.length === 0) {
+        return node
+      }
+
+      if(!node) {
+        node = new ListNode(list[0])
+      } else {
+        node.val = list[0]
+      }
+
+      if (list.length === 1) {
+        node.next = null
+      } else {
+        node.next = new ListNode()
+      }
+
+      return _listToNodeList(list.slice(1), node.next)
+    }
+    const firstNode = new ListNode()
+    _listToNodeList(list, firstNode)
+
+    return firstNode
+  }
+
+
+
+  return listToNodeList([...listNodeToList(list1), ...listNodeToList(list2)].sort())
 };
 // @lc code=end
