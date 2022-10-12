4fed1a2fcd64f1434138ea949f440f3d9601bb2e
kawamataryo <ba068082@gmail.com>
2022-10-13 08:25:14 +0900

    [from now] 2022/10/13 08:25:14
    diff --git a/solutions/70.climbing-stairs.py b/solutions/70.climbing-stairs.py
    index 88ef478..2bcfdeb 100644
    --- a/solutions/70.climbing-stairs.py
    +++ b/solutions/70.climbing-stairs.py
    @@ -7,10 +7,13 @@
     # @lc code=start
     class Solution:
         def climbStairs(self, n: int) -> int:
    -        # 1檀か2段
    +        dp = [1] + [0] * n
    
    -        return 1
    -        # n
    -        # n / 2
    -        #
    +        # n = n if n <= 2
    +        # n = n(n-1) + n(n-2) if n > 2
    +        for i in range(1, n + 1):
    +            dp[i] += dp[i - 1] if i >= 1 else 0
    +            dp[i] += dp[i - 2] if i >= 2 else 0
    +
    +        return dp[n]
     # @lc code=end

--- solutions/70.climbing-stairs.py
+++ solutions/70.climbing-stairs.py
@@ -7,10 +7,13 @@
 # @lc code=start
 class Solution:
     def climbStairs(self, n: int) -> int:
-        # 1檀か2段
+        dp = [1] + [0] * n
 
-        return 1
-        # n
-        # n / 2
-        #
+        # n = n if n <= 2
+        # n = n(n-1) + n(n-2) if n > 2
+        for i in range(1, n + 1):
+            dp[i] += dp[i - 1] if i >= 1 else 0
+            dp[i] += dp[i - 2] if i >= 2 else 0
+
+        return dp[n]
 # @lc code=end
