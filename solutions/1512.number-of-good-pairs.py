0c350f5f88ce533340c4e116d157d09abed062a8
kawamataryo <ba068082@gmail.com>
2022-10-17 08:06:36 +0900

    [from now] 2022/10/17 08:06:36
    diff --git a/solutions/1512.number-of-good-pairs.py b/solutions/1512.number-of-good-pairs.py
    index 7ae2162..edfdf02 100644
    --- a/solutions/1512.number-of-good-pairs.py
    +++ b/solutions/1512.number-of-good-pairs.py
    @@ -7,12 +7,27 @@
     # @lc code=start
     class Solution:
         def numIdenticalPairs(self, nums: List[int]) -> int:
    +        # My first solution.
    +        # -------------------------
    +        # good_pair_count = 0
    +        # for x in range(len(nums)):
    +        #     for y in range (x + 1, len(nums)):
    +        #         if(nums[x] == nums[y]):
    +        #             good_pair_count+= 1
    +        # return good_pair_count
    +
    +        # (WIP) My second solutions.
    +        # 15 min
    +        # -------------------------
    +        hash_table = {}
    +        for i in range(len(nums)):
    +            # FIXME: 同じ数が複数あることを考慮できていない
    +            hash_table[nums[i]] = i
    +
             good_pair_count = 0
    -        for x in range(len(nums)):
    -            for y in range (x + 1, len(nums)):
    -                if(nums[x] == nums[y]):
    -                    good_pair_count+= 1
    +        for i in range(len(nums)):
    +            if nums[i] in hash_table and i < hash_table[nums[i]]:
    +                good_pair_count += 1
    
             return good_pair_count
    -
     # @lc code=end

--- solutions/1512.number-of-good-pairs.py
+++ solutions/1512.number-of-good-pairs.py
@@ -7,12 +7,27 @@
 # @lc code=start
 class Solution:
     def numIdenticalPairs(self, nums: List[int]) -> int:
+        # My first solution.
+        # -------------------------
+        # good_pair_count = 0
+        # for x in range(len(nums)):
+        #     for y in range (x + 1, len(nums)):
+        #         if(nums[x] == nums[y]):
+        #             good_pair_count+= 1
+        # return good_pair_count
+
+        # (WIP) My second solutions.
+        # 15 min
+        # -------------------------
+        hash_table = {}
+        for i in range(len(nums)):
+            # FIXME: 同じ数が複数あることを考慮できていない
+            hash_table[nums[i]] = i
+
         good_pair_count = 0
-        for x in range(len(nums)):
-            for y in range (x + 1, len(nums)):
-                if(nums[x] == nums[y]):
-                    good_pair_count+= 1
+        for i in range(len(nums)):
+            if nums[i] in hash_table and i < hash_table[nums[i]]:
+                good_pair_count += 1
 
         return good_pair_count
-
 # @lc code=end
