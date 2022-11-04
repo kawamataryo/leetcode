39b9c4d2aed90675c5b3a9908144ae199f3ce9b4
kawamataryo <ba068082@gmail.com>
2022-11-04 17:22:57 +0900

    [from now] 2022/11/04 17:22:57
    diff --git a/solutions/832.flipping-an-image.py b/solutions/832.flipping-an-image.py
    index e3b7e64..83cc41c 100644
    --- a/solutions/832.flipping-an-image.py
    +++ b/solutions/832.flipping-an-image.py
    @@ -9,9 +9,7 @@ class Solution:
         def flipAndInvertImage(self, image: List[List[int]]) -> List[List[int]]:
             for row in image:
                 for i in range(math.ceil(len(row) / 2)):
    -                tmp = row[i]
    -                row[i] = row[-i-1] ^ 1
    -                row[-i-1] = tmp ^ i
    +                row[i], row[-i-1] = row[-i-1] ^ 1, row[i] ^ 1
    
             return image
     # @lc code=end

--- solutions/832.flipping-an-image.py
+++ solutions/832.flipping-an-image.py
@@ -9,9 +9,7 @@ class Solution:
     def flipAndInvertImage(self, image: List[List[int]]) -> List[List[int]]:
         for row in image:
             for i in range(math.ceil(len(row) / 2)):
-                tmp = row[i]
-                row[i] = row[-i-1] ^ 1
-                row[-i-1] = tmp ^ i
+                row[i], row[-i-1] = row[-i-1] ^ 1, row[i] ^ 1
 
         return image
 # @lc code=end
