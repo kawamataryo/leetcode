ad96e44d88f9f5a37bd8fc4f76771f124af23ef0
kawamataryo <ba068082@gmail.com>
2023-01-06 10:37:11 +0900

    [from now] 2023/01/06 10:37:11
    diff --git a/solutions/733.flood-fill.py b/solutions/733.flood-fill.py
    index aacd5e3..965816a 100644
    --- a/solutions/733.flood-fill.py
    +++ b/solutions/733.flood-fill.py
    @@ -5,28 +5,7 @@
     #
    
     # @lc code=start
    -from collections import deque
    -
     class Solution:
         def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
    -        if image[sr][sc] == color:
    -            return image
    -
    -        q = deque([(sr, sc)])
    -        while q:
    -            r, c = q.popleft()
    -            old_color, image[r][c] = image[r][c], color
    -
    -            if old_color == color:
    -                continue
    -            if r < len(image) - 1 and image[r + 1][c] == old_color:
    -                q.append((r + 1, c))
    -            if r > 0 and image[r - 1][c] == old_color:
    -                q.append((r - 1, c))
    -            if c < len(image[0]) - 1 and image[r][c + 1] == old_color:
    -                q.append((r, c + 1))
    -            if c > 0 and image[r][c - 1] == old_color:
    -                q.append((r, c - 1))
    -        return image
    
     # @lc code=end

--- solutions/733.flood-fill.py
+++ solutions/733.flood-fill.py
@@ -5,28 +5,7 @@
 #
 
 # @lc code=start
-from collections import deque
-
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-        if image[sr][sc] == color:
-            return image
-
-        q = deque([(sr, sc)])
-        while q:
-            r, c = q.popleft()
-            old_color, image[r][c] = image[r][c], color
-
-            if old_color == color:
-                continue
-            if r < len(image) - 1 and image[r + 1][c] == old_color:
-                q.append((r + 1, c))
-            if r > 0 and image[r - 1][c] == old_color:
-                q.append((r - 1, c))
-            if c < len(image[0]) - 1 and image[r][c + 1] == old_color:
-                q.append((r, c + 1))
-            if c > 0 and image[r][c - 1] == old_color:
-                q.append((r, c - 1))
-        return image
 
 # @lc code=end
